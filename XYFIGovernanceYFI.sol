281c95692c7f6817b4e4ed5d618ea1bff0c04299
XYearnFinance <71262222+XYearnFinance@users.noreply.github.com>
2020-09-14 20:36:05 +0800

    XYFI Gov Contract Source Code

--- XYFIGovernanceYFI.sol
+++ XYFIGovernanceYFI.sol
@@ -32,7 +32,7 @@ pragma solidity ^0.5.0;
  */
 library Math {
     /**
-     * @dev Returns the largest of two numbers.
+     * @dev Returns the largest of two numbers. 
      */
     function max(uint256 a, uint256 b) internal pure returns (uint256) {
         return a >= b ? a : b;
